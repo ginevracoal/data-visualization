555682759804ec3b489dacc93320286481e5c171
doma <domagoj.korais@gmail.com>
2018-12-06 18:04:17 +0100

    Sentiment plots done

--- exam/economist_analysis.R
+++ exam/economist_analysis.R
@@ -2,6 +2,7 @@ library(feather)
 library(tidyverse)
 library(lubridate)
 library(ggthemes)
+library(tidytext)
 
 #load db and separate in different years, primary key given by year, number_edition, number_article
 df=read_feather(path="my_data2.feather")
@@ -36,8 +37,101 @@ ggplot(politicalDf,aes(x=date,fill=as.factor(flag)))+
            y=0.5,label="End ")+
   theme_economist()
   
-  
 
+#Let's start with the sentyment analysis
+
+#First some data cleaning
+
+# Clean text from wrongly parsed html
+politicalDf$article=gsub("Get 3 free articles per week, daily \n newsletters and more.","",politicalDf$article)
+
+tidyPoliticalDf=politicalDf%>%
+  unnest_tokens(word,article)
+
+#removing stop words
+data("stop_words")
+tidyNoStopPoliticalDf=tidyPoliticalDf%>%
+  anti_join(stop_words)
+
+cw=tidyNoStopPoliticalDf%>%
+  group_by(flag,year)%>%
+  count(word,sort = TRUE)
+
+#Plotting word frequency
+tidyNoStopPoliticalDf%>%
+  count(word, sort = TRUE) %>%
+  filter(n > 2000) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n)) +
+  geom_col() +
+  xlab(NULL) +
+  coord_flip()
+
+#dataset from tidytext
+sentiments
+
+nrc=get_sentiments(lexicon = "nrc")
+afinn=get_sentiments(lexicon = "afinn")
+bing=get_sentiments(lexicon = "bing")
+loughran=get_sentiments(lexicon = "loughran")
+
+#let's search for different sentiments
+
+politicalSentiment=inner_join(tidyNoStopPoliticalDf,bing,by = "word")
+
+#I want to plot sentiment as function of time like before!
+#plotting relative frequencies
+politicalSentiment%>%
+  ggplot(aes(x=date,fill=as.factor(sentiment)))+
+  geom_histogram(position = "fill",binwidth = 90)+
+  facet_grid("flag")+
+  labs(x="Date (bins are 50 days)",
+       y="Normalized frequency",
+       title="Clinton vs Trump bing lexicon",
+       fill="",
+       caption = "Based on the online european editions of \"The Economist\"")+
+  annotate(geom="text",x=as.Date("2015-06-16"),
+           y=0.5,label="Start ")+
+  annotate(geom="text",x=as.Date("2016-11-08"),
+           y=0.5,label="End ")+
+  theme_economist()+
+  scale_fill_gdocs()
+
+
+politicalSentiment=inner_join(tidyNoStopPoliticalDf,loughran,by = "word")
+politicalSentiment%>%
+  ggplot(aes(x=date,fill=as.factor(sentiment)))+
+  geom_histogram(position = "fill",binwidth = 90)+
+  facet_grid("flag")+
+  labs(x="Date (bins are 50 days)",
+       y="Normalized frequency",
+       title="Clinton vs Trump Loughran lexicon",
+       fill="",
+       caption = "Based on the online european editions of \"The Economist\"")+
+  annotate(geom="text",x=as.Date("2015-06-16"),
+           y=0.5,label="Start ")+
+  annotate(geom="text",x=as.Date("2016-11-08"),
+           y=0.5,label="End ")+
+  theme_economist()+
+  scale_fill_gdocs()
+
+
+politicalSentiment=inner_join(tidyNoStopPoliticalDf,nrc,by = "word")
+politicalSentiment%>%
+  ggplot(aes(x=date,fill=as.factor(sentiment)))+
+  geom_histogram(position = "fill",binwidth = 90)+
+  facet_grid("flag")+
+  labs(x="Date (bins are 50 days)",
+       y="Normalized frequency",
+       title="Clinton vs Trump nrc lexicon",
+       fill="",
+       caption = "Based on the online european editions of \"The Economist\"")+
+  annotate(geom="text",x=as.Date("2015-06-16"),
+           y=0.5,label="Start ")+
+  annotate(geom="text",x=as.Date("2016-11-08"),
+           y=0.5,label="End ")+
+  theme_economist()+
+  scale_fill_gdocs()
 
 
 
